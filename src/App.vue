b14c0a15eb6e2ff1f624cacb8bdb9f758b8caf6d
tfpresentes <83709963+tfpresentes@users.noreply.github.com>
2021-05-29 12:39:58 -0300

    bg

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,12 @@ export default {
 <style>
 #app {
   background-image: linear-gradient(to bottom, #EB4C00, #F2A900);
+  padding-top: var(--header-height);
+  transition: opacity .7s cubic-bezier(.4,0,.2,1);
+  box-sizing: border-box;
+  display: block;
+  --home-max-width: 100vw;
+  --gutter-width: 15px;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
