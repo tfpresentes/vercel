eda4b0752787b0bf3109f8fdafef3f7743465b3c
tfpresentes <83709963+tfpresentes@users.noreply.github.com>
2021-05-29 12:30:52 -0300

    blank page only logo

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,7 @@ export default {
 
 <style>
 #app {
+  background-image: linear-gradient(to bottom, #EB4C00, #F2A900);
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
