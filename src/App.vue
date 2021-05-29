706d7374fd26233692453dc3017f751cde8d0b3e
Renato Miranda <renatomirandagoncalves@outlook.com>
2021-05-29 18:06:18 -0300

    new start

--- src/App.vue
+++ src/App.vue
@@ -48,18 +48,15 @@ export default {
 
 <style>
 #app {
-  background-image: linear-gradient(to bottom, #EB4C00, #F2A900);
+  background-image: linear-gradient(135deg,#eb4c00,#f2a900);
   padding-top: var(--header-height);
   transition: opacity .7s cubic-bezier(.4,0,.2,1);
-  box-sizing: border-box;
-  display: block;
+  display: flex;
   --home-max-width: 100vw;
   --gutter-width: 15px;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
