22908190eef7c53edfe8f6e005be8007c76f872e
tfpresentes <83709963+tfpresentes@users.noreply.github.com>
2021-05-29 12:50:04 -0300

    bg

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="iShopee logo" src="./assets/logoIshopee.png">
+    <HelloWorld msg="Cresça em volume"/>
   </div>
 </template>
 
