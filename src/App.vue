4ac523c2936b50da1e40e2639aa8d6880902c584
Renato Miranda <renatomirandagoncalves@outlook.com>
2021-05-30 13:47:42 -0300

    basic webpage v0.2

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
     <TopBar msg="Cresça em volume"/>
-    <div class="bg">
-      <img src="./assets/bg.svg" alt="background image" class="background">
-    </div>
+    <Body msg="iShopee"/>
   </div>
 </template>
 
 <script>
 import TopBar from './components/TopBar.vue'
+import Body from './components/Body.vue'
+
 
 export default {
   name: 'app',
   components: {
-    TopBar
+    TopBar,
+    Body
   }
 }
 </script>
@@ -23,12 +24,10 @@ body, html {
   height: 100%;
   width: 100%;
 }
-.bg {
-  background-image: "./assets/bg.svg";
-}
 #app {
   background-image: linear-gradient(135deg,#eb4c00,#f2a900);
   height: 100%;
   width: 100%;
+  margin: 0em;
 }
 </style>
---
+++ src/components/Body.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+      <img src="../assets/bg.svg" alt="background image">
+    </div>
+</template>
